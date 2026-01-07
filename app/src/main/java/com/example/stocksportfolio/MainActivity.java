760e72ac0a520fa98d1ffd3484a83d0d50ae1847
Neev Chordeker <neevch19@gmail.com>
2026-01-07 15:12:59 +0200

    connected app to realtime DB and added write and read functions

--- app/src/main/java/com/example/stocksportfolio/MainActivity.java
+++ app/src/main/java/com/example/stocksportfolio/MainActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -63,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(MainActivity.this, "Registration successful", Toast.LENGTH_LONG).show();
+                            writeToDB();
                             NavHostFragment navFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
                             navFragment.getNavController().navigate(R.id.action_register2_to_logIn2);
                         } else {
@@ -70,5 +76,40 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public void writeToDB(){
+        String email = ((EditText)findViewById(R.id.NewEmail)).getText().toString();
+        String password = ((EditText)findViewById(R.id.NewPass)).getText().toString();
+        String name = ((EditText)findViewById(R.id.Name)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.LastName)).getText().toString();
+        String phone = ((EditText)findViewById(R.id.Phone)).getText().toString();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users").child(phone);
+        User user = new User(email, password, name, lastName, phone);
+        myRef.setValue(user);
+    }
+
+    public void readFromDB() {
+        String email = ((EditText)findViewById(R.id.NewEmail)).getText().toString();
+        String password = ((EditText)findViewById(R.id.NewPass)).getText().toString();
+        String name = ((EditText)findViewById(R.id.Name)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.LastName)).getText().toString();
+        String phone = ((EditText)findViewById(R.id.Phone)).getText().toString();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users").child(phone);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                User value = dataSnapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Error" + error, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
